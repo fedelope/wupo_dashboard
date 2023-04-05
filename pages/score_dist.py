3769f91b66244c0b516b0092e48e908c6e01c3fa
fedelope <fed.lope@gmial.com>
2023-04-05 10:58:32 -0500

    DashTools Automatic Push - 04/05/2023 @ 10:58:31

--- pages/score_dist.py
+++ pages/score_dist.py
@@ -24,7 +24,8 @@ from scipy.stats import norm
 import plotly.figure_factory as ff
 
 # Read data
-df = pd.read_csv("interactive_kpi/dash_env/lib/python3.8/site-packages/auto_KPI_app/data/kpi_platicapp.csv")
+#df = pd.read_csv("interactive_kpi/dash_env/lib/python3.8/site-packages/auto_KPI_app/data/kpi_platicapp.csv")
+df = pd.read_csv("https://github.com/fedelope/wupo_dashboard/blob/3bb81a3d76630665621e19dd5f58bf196db6f9b5/data/kpi_platicapp.csv")
 print(df[ :15])
 
 dash.register_page(__name__, path='/pages/score_dist', name='Distribución SCORE')
