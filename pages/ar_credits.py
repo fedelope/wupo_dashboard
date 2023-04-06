f0beea07b3ac28c798b2dcc69f0cbfebf29f3f93
fedelope <fed.lope@gmial.com>
2023-04-05 22:05:21 -0500

    homepage-fix_2

--- pages/ar_credits.py
+++ pages/ar_credits.py
@@ -19,7 +19,7 @@ import plotly.graph_objects as go
 
 
 
-dash.register_page(__name__, path='/|/pages/ar_credits', name='Créditos') # '/' is home page
+dash.register_page(__name__, path='/' or '/pages/ar_credits', name='Créditos') # '/' is home page
 
 # page_1 - Macro data
 df = pd.read_csv("../data/kpi_platicapp.csv")
